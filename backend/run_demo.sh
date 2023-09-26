49cb69cc6be2527762d0c96b3260e65e25fd5eb8
Kmilia <camelia.guerraoui@gmail.com>
2023-09-26 18:21:03 +0900

    Refactor Backend Folder

--- backend/run_demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-source ~/.zshrc
-cd ../frontvue/
-nvm use 16.14.2
-npm run build
-cd ../backend/
-export FLASK_APP=main.py
-python3 -m flask run
---
+++ run_demo.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+export FLASK_APP=backend/main.py && python3 -m flask run
