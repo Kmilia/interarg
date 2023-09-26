e41c498ca974d98bb8884ffc562de42682ece29e
Kmilia <camelia.guerraoui@gmail.com>
2023-09-26 15:12:58 +0900

    Hide Api Keys

--- backend/model_gpt4.py
+++ backend/model_gpt4.py
@@ -1,8 +1,10 @@
 import openai
 import shortuuid
+import pandas as pd
 
-openai.api_key = "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"
-openai.organization = "org-0M5WYRn7SjJnnKLbFybd5QUE"
+_KEYS = pd.read_csv("openai_keys/openai_id.csv").to_dict("records")[0]
+openai.api_key = _KEYS["openai.api_key"]
+openai.organization = _KEYS["openai.organization"]
 
 def get_feedback(paragraph):
     prompt = f"Give a feedback on how to improve this argument '{paragraph}'"
@@ -22,7 +24,7 @@ def send_prompt(prompt, paragraph, max_token=500, outputs=1):
     uuid = shortuuid.uuid()
     alert = [{"id":uuid, "help": "Suggestion", "action": "Consider this feedback:", 'detailed': " ".join(output)}]
     html = f"<span class='error'><span id={uuid}>{paragraph}</span></span>"
-    
+
     return {'html': html, 'alerts': alert}
 
 #test_para = "I love uniforms because everyone looks the same. So it removes inequalities among students. "
